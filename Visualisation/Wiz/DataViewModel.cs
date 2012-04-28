7769ffdf123a99c09b6c749cc20a769b90bc1738
Lukasz Madon <lukasz.madon@gmail.com>
2012-04-28 17:25:58 +0200

    Visualiation initial

---
+++ Visualisation/Wiz/DataViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Wiz
+{
+    public class SeriesViewModel
+    {
+        private WebClient webClient = new WebClient();
+
+        private ObservableCollection<KeyValuePair<double, double>> series = new ObservableCollection<KeyValuePair<double, double>>();
+
+        public SeriesViewModel(Uri seriesUri)
+        {
+            webClient.DownloadStringCompleted += webClient_DownloadStringCompleted;
+            webClient.DownloadStringAsync(seriesUri);
+        }
+
+        private void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            var result = e.Result;
+            var lines = result.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                var data = line.Split(' ');
+                var x = Double.Parse(data[0], CultureInfo.InvariantCulture);
+                var y = Double.Parse(data[1], CultureInfo.InvariantCulture);
+                var xyPair = new KeyValuePair<double, double>(x, y);
+                series.Add(xyPair);
+            }
+        }
+
+        public ObservableCollection<KeyValuePair<double, double>> Series
+        {
+            get { return series; }
+        }
+    }
+}
---
+++ Visualisation/Wiz/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Browser;
+using System.Windows.Controls;
+
+namespace Wiz
+{
+    public partial class MainPage : UserControl
+    {
+        private const string urlParam = "url";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var parameters = HtmlPage.Document.QueryString;
+            if (parameters.ContainsKey(urlParam))
+            {
+                var url = parameters[urlParam];
+                Uri fileUri;
+                if (Uri.TryCreate(url, UriKind.Absolute, out fileUri))
+                {
+                    this.DataContext = new SeriesViewModel(fileUri);
+                    urlTextBox.Text = url;
+                }
+                else
+                {
+                    urlTextBox.Text = "malformed simulation data url";
+                }
+            }
+            else
+            {
+                urlTextBox.Text = "No file location in url";
+            }
+        }
+    }
+}
